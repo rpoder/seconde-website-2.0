9958f79c1edd33b91ee252bd5ba6f48f6a5f5675
Ronan <rpoder@student.42.fr>
2024-11-09 21:36:34 +0100

    fix: roadmap style on phones

--- src/app/roadmap/page.tsx
+++ src/app/roadmap/page.tsx
@@ -18,9 +18,12 @@ export default function page() {
 					{format(updatedAt, "dd LLLL yyyy", { locale: fr })}
 				</p>
 			</div>
-			<div className="flex flex-row gap-12">
+			<div className="flex flex-col md:flex-row gap-12">
 				{collumns.map((column, index) => (
-					<div key={index} className="flex flex-col w-full gap-8">
+					<div
+						key={index}
+						className="flex flex-col w-full md:gap-8 gap-2"
+					>
 						<h2 className="border px-4 py-4">{column.title}</h2>
 						<div className="flex flex-col">
 							{column.items.map((item, index) => (
