8ce074f321405e2ac6539066e4972e010847b97b
Ronan <rpoder@student.42.fr>
2025-02-05 16:38:23 +0100

    feat: add partners

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,13 @@ import Pricing from "@/components/sections/index/pricing.section";
 import FeaturesBis from "@/components/sections/index/features-bis.section";
 import Explore from "@/components/sections/index/explore.section";
 import SoftwareFeatures from "@/components/sections/index/software-features.section";
+import Reinsurance from "@/components/sections/index/reinsurance.section";
 
 export default function Home() {
 	return (
 		<>
 			<Hero />
+			<Reinsurance />
 			<Features />
 			<Explore />
 			<Pricing />
--- src/components/sections/index/reinsurance.section.tsx
+++ src/components/sections/index/reinsurance.section.tsx
@@ -1,5 +1,59 @@
 import React from "react";
+import LogoLaMome from "@/../public/assets/svg/logos_partners/logo_partner_la_môme.svg";
+import LogoOccasionnel from "@/../public/assets/svg/logos_partners/logo_partner_occsionnel.svg";
+import LogoUrbanExchange from "@/../public/assets/svg/logos_partners/logo_partner_urban_exchange.svg";
+import LogoLesTresorsDeCaro from "@/../public/assets/svg/logos_partners/logo_partner_trésors_de_caro.svg";
+import Image, { StaticImageData } from "next/image";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 export default function Reinsurance() {
-	return <div>Reinsurance</div>;
+	const partners: {
+		name: string;
+		logo: StaticImageData;
+		url: string;
+		className?: string;
+	}[] = [
+		{
+			name: "La Môme",
+			logo: LogoLaMome,
+			url: "https://www.instagram.com/lamomemarseille/",
+		},
+		{
+			name: "Urban Exchange",
+			logo: LogoUrbanExchange,
+			url: "https://www.instagram.com/urbanexchangeparis/",
+			className: "w-20",
+		},
+		{
+			name: "Occasionnel",
+			logo: LogoOccasionnel,
+			url: "https://www.instagram.com/occasionnel.luxe/",
+			className: "w-36",
+		},
+
+		{
+			name: "Les Trésors de Caro",
+			logo: LogoLesTresorsDeCaro,
+			url: "https://www.instagram.com/lestresorsdecaro_depotvente/",
+		},
+	];
+
+	return (
+		<div className="bg-secondary py-8">
+			<div className="w-full flex md:flex-row flex-col gap-12 items-center justify-center opacity-70">
+				{partners.map((partner, index) => (
+					<div key={index} className="px-4 py-2 h-full ">
+						<Link href={partner.url} target="_blank">
+							<Image
+								src={partner.logo}
+								alt={partner.name}
+								className={cn("w-28", partner.className)}
+							/>
+						</Link>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
